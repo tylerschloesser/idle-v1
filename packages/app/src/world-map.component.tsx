f505d3bd54148f36310ae68b36f2b44cbc6a8282
Tyler Schloesser <tylerschloesser@gmail.com>
2024-01-19 19:35:26 -0800

    refactor resize observer

--- packages/app/src/world-map.component.tsx
+++ packages/app/src/world-map.component.tsx
@@ -3,39 +3,44 @@ import invariant from 'tiny-invariant'
 import styles from './world-map.module.scss'
 
 export function WorldMap() {
-  const containerRef = useRef<HTMLDivElement | null>(null)
   const canvasRef = useRef<HTMLCanvasElement | null>(null)
 
   useEffect(() => {
-    const container = containerRef.current
-    invariant(container)
+    const controller = new AbortController()
     const canvas = canvasRef.current
     invariant(canvas)
-
-    const ro = new ResizeObserver((entries) => {
-      invariant(entries.length === 1)
-      const entry = entries.at(0)
-      invariant(entry)
-      invariant(canvas)
-
-      const { contentRect: rect } = entry
-
-      canvas.width = rect.width
-      canvas.height = rect.height
-    })
-    ro.observe(container)
+    initCanvas(canvas, controller.signal)
 
     return () => {
-      ro.disconnect()
+      controller.abort()
     }
   }, [])
 
   return (
-    <div className={styles.container} ref={containerRef}>
-      <canvas
-        className={styles.canvas}
-        ref={canvasRef}
-      ></canvas>
-    </div>
+    <canvas
+      className={styles.canvas}
+      ref={canvasRef}
+    ></canvas>
   )
+}
+
+function initCanvas(
+  canvas: HTMLCanvasElement,
+  signal: AbortSignal,
+) {
+  const ro = new ResizeObserver((entries) => {
+    invariant(entries.length === 1)
+    const entry = entries.at(0)
+    invariant(entry)
+    invariant(canvas)
+
+    const { contentRect: rect } = entry
+
+    canvas.width = rect.width
+    canvas.height = rect.height
+  })
+  ro.observe(canvas)
+  signal.addEventListener('abort', () => {
+    ro.disconnect()
+  })
 }
