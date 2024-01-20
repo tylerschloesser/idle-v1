a5c83141fb2de256e65b0d1cbe26d75ac2ec3ce1
Tyler Schloesser <tylerschloesser@gmail.com>
2024-01-19 20:21:44 -0800

    add draw chunk

--- packages/app/src/world-map.component.tsx
+++ packages/app/src/world-map.component.tsx
@@ -1,16 +1,19 @@
-import { useEffect, useRef } from 'react'
+import { useContext, useEffect, useRef } from 'react'
 import invariant from 'tiny-invariant'
+import { Context } from './context.js'
 import styles from './world-map.module.scss'
+import { World } from './world.js'
 
 export function WorldMap() {
   const canvasRef = useRef<HTMLCanvasElement | null>(null)
+  const { world } = useContext(Context)
 
   useEffect(() => {
     const controller = new AbortController()
     const canvas = canvasRef.current
     invariant(canvas)
     initResizeObserver(canvas, controller.signal)
-    initRenderLoop(canvas, controller.signal)
+    initRenderLoop(canvas, controller.signal, world)
     return () => {
       controller.abort()
     }
@@ -32,18 +35,37 @@ function getContext(
   return context
 }
 
+function drawChunk(
+  x: number,
+  y: number,
+  world: World,
+): void {
+  const id = `${x}.${y}`
+  const chunk = world.chunks[id]
+  invariant(chunk)
+}
+
 function initRenderLoop(
   canvas: HTMLCanvasElement,
   signal: AbortSignal,
+  world: World,
 ): void {
   const context = getContext(canvas)
   function render() {
     if (signal.aborted) {
       return
     }
+
     context.clearRect(0, 0, canvas.width, canvas.height)
     context.fillStyle = 'red'
     context.fillRect(0, 0, 100, 100)
+
+    for (let y = -1; y <= 0; y++) {
+      for (let x = -1; x <= 0; x++) {
+        drawChunk(x, y, world)
+      }
+    }
+
     window.requestAnimationFrame(render)
   }
   window.requestAnimationFrame(render)
