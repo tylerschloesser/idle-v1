5a2cd3aed0e97e20fde08470e16e616c941823cf
Tyler Schloesser <tylerschloesser@gmail.com>
2024-01-19 18:44:18 -0800

    add world page

--- packages/app/src/app.component.tsx
+++ packages/app/src/app.component.tsx
@@ -4,11 +4,16 @@ import {
 } from 'react-router-dom'
 import styles from './app.module.scss'
 import { RootPage } from './root-page.component.js'
+import { WorldPage } from './world.component.js'
 
 const router = createBrowserRouter([
   {
     index: true,
     Component: RootPage,
+  },
+  {
+    path: 'world',
+    Component: WorldPage,
   },
 ])
 
