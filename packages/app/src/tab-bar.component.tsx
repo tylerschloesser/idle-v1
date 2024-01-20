69d0d258809d219df85df8222a2d3489e31b9e11
Tyler Schloesser <tylerschloesser@gmail.com>
2024-01-20 09:40:52 -0800

    update href

--- packages/app/src/tab-bar.component.tsx
+++ packages/app/src/tab-bar.component.tsx
@@ -1,28 +1,45 @@
+import { useContext } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { Context } from './context.js'
 import styles from './tab-bar.module.scss'
 
 const tabs = [
   {
     label: 'Mine',
+    path: 'mine',
   },
   {
     label: 'Build',
+    path: 'build',
   },
   {
     label: 'Stats',
+    path: 'stats',
   },
   {
     label: 'Settings',
+    path: 'settings',
   },
 ]
 
 export function TabBar() {
+  const { world } = useContext(Context)
+  const navigate = useNavigate()
   return (
     <div className={styles['tab-bar']}>
-      <div className={styles.list}>
-        {tabs.map(({ label }) => (
-          <div key={label} className={styles.item}>
+      <div className={styles.fixed}>
+        {tabs.map(({ label, path }) => (
+          <a
+            href={`/world/${world.id}/${path}`}
+            key={label}
+            className={styles.item}
+            onClick={(e) => {
+              e.preventDefault()
+              navigate(`/world/${world.id}/${path}`)
+            }}
+          >
             {label}
-          </div>
+          </a>
         ))}
       </div>
     </div>
