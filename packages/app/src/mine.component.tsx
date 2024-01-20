f11a96301441e4be134d925477276cb73ed3ab00
Tyler Schloesser <tylerschloesser@gmail.com>
2024-01-19 21:43:53 -0800

    add mine button

--- packages/app/src/mine.component.tsx
+++ packages/app/src/mine.component.tsx
@@ -1,3 +1,37 @@
+import styles from './mine.module.scss'
+
+function MineButton() {
+  return (
+    <button>
+      <span>Mine</span>
+      <input
+        className={styles.checkbox}
+        type="checkbox"
+      ></input>
+    </button>
+  )
+}
+
 export function Mine() {
-  return <>Mine</>
+  const resources = [
+    {
+      label: 'Coal',
+      color: 'black',
+    },
+    {
+      label: 'Stone',
+      color: 'sand',
+    },
+  ]
+
+  return (
+    <>
+      {resources.map(({ label, color }, i) => (
+        <div key={i}>
+          <div>{label}</div>
+          <MineButton />
+        </div>
+      ))}
+    </>
+  )
 }
