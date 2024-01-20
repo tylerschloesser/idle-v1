82302a0ece0a202b0267caf8f5269c931e68a45b
Tyler Schloesser <tylerschloesser@gmail.com>
2024-01-19 18:37:11 -0800

    add hipster impsum

--- packages/app/src/root-page.component.tsx
+++ packages/app/src/root-page.component.tsx
@@ -4,7 +4,7 @@ export function RootPage() {
   return (
     <>
       <div className={styles.main}>
-        <h1 className={styles.header}>idle-v1</h1>
+        <h1 className={styles.h1}>idle-v1</h1>
         <div className={styles['bottom-container']}>
           <div className={styles['scroll-down']}>
             <span>Scroll down to learn more</span>
@@ -16,7 +16,13 @@ export function RootPage() {
         </div>
       </div>
       <div className={styles.about}>
-        <h2>About</h2>
+        <h2 className={styles.h2}>About</h2>
+        <p className={styles.p}>
+          I'm baby shoreditch copper mug pork belly yuccie
+          kitsch single-origin coffee cupping. Direct trade
+          pickled edison bulb fingerstache, street art
+          occupy tumeric enamel pin yuccie jean shorts.
+        </p>
       </div>
     </>
   )
