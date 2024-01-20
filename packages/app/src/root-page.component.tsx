b89e7d2f398886a080f10707ea7262dd457a8af4
Tyler Schloesser <tylerschloesser@gmail.com>
2024-01-19 18:41:16 -0800

    fix button

--- packages/app/src/root-page.component.tsx
+++ packages/app/src/root-page.component.tsx
@@ -1,6 +1,8 @@
+import { useNavigate } from 'react-router-dom'
 import styles from './root-page.module.scss'
 
 export function RootPage() {
+  const navigate = useNavigate()
   return (
     <>
       <div className={styles.main}>
@@ -10,17 +12,17 @@ export function RootPage() {
             <span>Scroll down to learn more</span>
             <span>or</span>
           </div>
-          <button className={styles['new-world-button']}>
+          <a className={styles['new-world-button']}>
             Start a new world
-          </button>
+          </a>
         </div>
       </div>
       <div className={styles.about}>
         <h2 className={styles.h2}>About</h2>
         <p className={styles.p}>
-          I'm baby shoreditch copper mug pork belly yuccie
-          kitsch single-origin coffee cupping. Direct trade
-          pickled edison bulb fingerstache, street art
+          I&apos;m baby shoreditch copper mug pork belly
+          yuccie kitsch single-origin coffee cupping. Direct
+          trade pickled edison bulb fingerstache, street art
           occupy tumeric enamel pin yuccie jean shorts.
         </p>
       </div>
