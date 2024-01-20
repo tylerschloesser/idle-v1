074006adb14e42251dce048c9189bf09a37fa9af
Tyler Schloesser <tylerschloesser@gmail.com>
2024-01-20 09:16:44 -0800

    update tab bar

--- packages/app/src/tab-bar.component.tsx
+++ packages/app/src/tab-bar.component.tsx
@@ -1,5 +1,30 @@
 import styles from './tab-bar.module.scss'
 
+const tabs = [
+  {
+    label: 'Mine',
+  },
+  {
+    label: 'Build',
+  },
+  {
+    label: 'Stats',
+  },
+  {
+    label: 'Settings',
+  },
+]
+
 export function TabBar() {
-  return <div className={styles['tab-bar']}>Tab bar</div>
+  return (
+    <div className={styles['tab-bar']}>
+      <div className={styles.list}>
+        {tabs.map(({ label }) => (
+          <div key={label} className={styles.item}>
+            {label}
+          </div>
+        ))}
+      </div>
+    </div>
+  )
 }
