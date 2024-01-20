5149089382579fa4d3b344694e082fadfdeb5d65
Tyler Schloesser <tylerschloesser@gmail.com>
2024-01-19 16:02:34 -0800

    add root page

--- packages/app/src/app.component.tsx
+++ packages/app/src/app.component.tsx
@@ -1,5 +1,21 @@
+import {
+  RouterProvider,
+  createBrowserRouter,
+} from 'react-router-dom'
 import styles from './app.module.scss'
+import { RootPage } from './root-page.component.js'
+
+const router = createBrowserRouter([
+  {
+    index: true,
+    Component: RootPage,
+  },
+])
 
 export function App() {
-  return <div className={styles.app}>TODO App</div>
+  return (
+    <div className={styles.app}>
+      <RouterProvider router={router} />
+    </div>
+  )
 }
