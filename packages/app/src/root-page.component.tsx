45d90e8cd51bf6126a158d015769a005107f857e
Tyler Schloesser <tylerschloesser@gmail.com>
2024-01-19 18:04:16 -0800

    refaoctr scroll down

--- packages/app/src/root-page.component.tsx
+++ packages/app/src/root-page.component.tsx
@@ -3,7 +3,21 @@ import styles from './root-page.module.scss'
 export function RootPage() {
   return (
     <>
-      <h1 className={styles.header}>idle-v1</h1>
+      <div className={styles.main}>
+        <h1 className={styles.header}>idle-v1</h1>
+        <div className={styles['bottom-container']}>
+          <div className={styles['scroll-down']}>
+            <span>Scroll down to learn more</span>
+            <span>or</span>
+          </div>
+          <button className={styles['new-world-button']}>
+            Start a new world
+          </button>
+        </div>
+      </div>
+      <div className={styles.about}>
+        <h2>About</h2>
+      </div>
     </>
   )
 }
