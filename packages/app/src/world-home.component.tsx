cd6bd27762e0152441c85539cb0800094cf7f1b7
Tyler Schloesser <tylerschloesser@gmail.com>
2024-01-20 10:19:20 -0800

    add id to world home

--- packages/app/src/world-home.component.tsx
+++ packages/app/src/world-home.component.tsx
@@ -1,9 +1,14 @@
+import { useContext } from 'react'
+import { Context } from './context.js'
+import styles from './world-home.module.scss'
 import { WorldMap } from './world-map.component.js'
 
 export function WorldHome() {
+  const { world } = useContext(Context)
   return (
     <>
       <WorldMap />
+      <p className={styles.p}>ID: {world.id}</p>
     </>
   )
 }
