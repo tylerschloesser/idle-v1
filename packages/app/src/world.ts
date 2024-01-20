47b00f4266d441e3f36d4760c85eacb625f6bc62
Tyler Schloesser <tylerschloesser@gmail.com>
2024-01-19 19:49:59 -0800

    add more types

--- packages/app/src/world.ts
+++ packages/app/src/world.ts
@@ -1,5 +1,19 @@
 import * as z from 'zod'
 
-export const World = z.strictObject({
+export const cellType = z.enum(['Grass1', 'Dirt1'])
+export type CellType = z.infer<typeof cellType>
+
+export const cell = z.strictObject({
+  type: cellType,
+})
+export type Cell = z.infer<typeof cell>
+
+export const chunk = z.strictObject({
   id: z.string(),
 })
+export type Chunk = z.infer<typeof chunk>
+
+export const world = z.strictObject({
+  id: z.string(),
+})
+export type World = z.infer<typeof world>
