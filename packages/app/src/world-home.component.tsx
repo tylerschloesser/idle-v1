5c7786c13fe649544447c753be0447b7453810a4
Tyler Schloesser <tylerschloesser@gmail.com>
2024-01-20 18:24:15 -0800

    render placeholder entity details

--- packages/app/src/world-home.component.tsx
+++ packages/app/src/world-home.component.tsx
@@ -2,13 +2,24 @@ import { useContext } from 'react'
 import { Context } from './context.js'
 import styles from './world-home.module.scss'
 import { WorldMap } from './world-map.component.js'
+import { Entity } from './world.js'
+
+function EntityDetails({ entity }: { entity: Entity }) {
+  return <div>{entity.type}</div>
+}
 
 export function WorldHome() {
   const { world } = useContext(Context)
+
+  const entities = Object.values(world.entities).flat()
+
   return (
     <>
       <WorldMap />
       <p className={styles.p}>ID: {world.id}</p>
+      {entities.map((entity, i) => (
+        <EntityDetails key={i} entity={entity} />
+      ))}
     </>
   )
 }
