0a852e6420d74cc72c13af95be9f1dfae49b9ad4
Tyler Schloesser <tylerschloesser@gmail.com>
2024-01-20 18:03:19 -0800

    add disabled

--- packages/app/src/world-build.component.tsx
+++ packages/app/src/world-build.component.tsx
@@ -1,10 +1,42 @@
+import { useContext } from 'react'
+import invariant from 'tiny-invariant'
+import { Context } from './context.js'
 import { WorldMap } from './world-map.component.js'
+import {
+  EntityType,
+  ItemType,
+  entityType,
+} from './world.js'
+
+function BuildEntity({ type }: { type: EntityType }) {
+  const { world } = useContext(Context)
+  const recipe = world.recipes[type]
+  invariant(recipe)
+  let disabled = false
+  for (const entry of Object.entries(recipe)) {
+    if (
+      (world.inventory[entry[0] as ItemType] ?? 0) <
+      entry[1]
+    ) {
+      disabled = true
+      break
+    }
+  }
+  return (
+    <div>
+      <div>{type}</div>
+      <button disabled={disabled}>Build</button>
+    </div>
+  )
+}
 
 export function WorldBuild() {
   return (
     <>
       <WorldMap />
-      <>TODO world build</>
+      {Object.values(entityType.enum).map((type) => (
+        <BuildEntity key={type} type={type} />
+      ))}
     </>
   )
 }
