266081fd7f38190accec1b0c867c0af294460d8e
Tyler Schloesser <tylerschloesser@gmail.com>
2024-01-22 11:47:37 -0800

    refactor text

--- packages/app/src/text.component.tsx
+++ packages/app/src/text.component.tsx
@@ -1,8 +1,25 @@
+import classNames from 'classnames'
 import styles from './text.module.scss'
 
 export type TextProps = React.PropsWithChildren<{
   variant?: 'b1'
+  invert?: boolean
+  bold?: boolean
 }>
-export function Text({ children }: TextProps) {
-  return <span className={styles.b1}>{children}</span>
+export function Text({
+  children,
+  invert,
+  bold,
+}: TextProps) {
+  return (
+    <span
+      className={classNames({
+        [styles.b1!]: true,
+        [styles.invert!]: invert,
+        [styles.bold!]: bold,
+      })}
+    >
+      {children}
+    </span>
+  )
 }
--- packages/app/src/world-home.component.tsx
+++ packages/app/src/world-home.component.tsx
@@ -7,13 +7,54 @@ import { Text } from './text.component.js'
 import styles from './world-home.module.scss'
 import { WorldMap } from './world-map.component.js'
 import {
+  BurnerMiningDrillEntity,
   EntityType,
   ItemType,
+  ResourceType,
   StoneFurnaceEntity,
 } from './world.js'
 
 function parseItemType(data: unknown): ItemType {
   return ItemType.parse(data)
+}
+
+function parseResourceType(data: unknown): ResourceType {
+  return ResourceType.parse(data)
+}
+
+function BurnerMiningDrillDetails({
+  entity,
+}: {
+  entity: BurnerMiningDrillEntity
+}) {
+  return (
+    <>
+      <Text>{entity.type}</Text>
+      <div></div>
+      <Select<ItemType>
+        placeholder="Choose Resource"
+        value={entity.resourceType}
+        onChange={() => {
+          console.log('todo')
+        }}
+        options={Object.values(ResourceType.Values)}
+        parse={parseResourceType}
+      />
+      <label className={styles.enabled}>
+        <Text invert bold>
+          Enabled
+        </Text>
+        <input
+          type="checkbox"
+          className={styles.checkbox}
+          checked={entity.enabled}
+          onChange={() => {
+            console.log('todo')
+          }}
+        />
+      </label>
+    </>
+  )
 }
 
 function StoneFurnaceDetails({
@@ -56,18 +97,19 @@ function StoneFurnaceDetails({
         <div className={styles['furnace-progress-fuel']} />
         <div className={styles['furnace-progress-craft']} />
       </div>
-      <div className={styles['furnace-controls']}>
-        <Select<ItemType>
-          placeholder="Choose Recipe"
-          value={entity.recipeItemType}
-          onChange={(itemType) => {
-            setStoneFurnaceRecipe(entity.id, itemType)
-          }}
-          options={Object.keys(world.furnaceRecipes).map(
-            parseItemType,
-          )}
-          parse={parseItemType}
-        />
+      <Select<ItemType>
+        placeholder="Choose Recipe"
+        value={entity.recipeItemType}
+        onChange={(itemType) => {
+          setStoneFurnaceRecipe(entity.id, itemType)
+        }}
+        options={Object.keys(world.furnaceRecipes).map(
+          parseItemType,
+        )}
+        parse={parseItemType}
+      />
+      <label className={styles.enabled}>
+        <Text>Enabled</Text>
         <input
           type="checkbox"
           className={styles.checkbox}
@@ -79,7 +121,7 @@ function StoneFurnaceDetails({
             )
           }}
         />
-      </div>
+      </label>
     </>
   )
 }
@@ -97,12 +139,19 @@ export function WorldHome() {
         <>
           <Heading3>Entities</Heading3>
           <div className={styles.grid}>
-            {entities.map((entity, i) => {
+            {entities.map((entity) => {
               switch (entity.type) {
                 case EntityType.enum.StoneFurnace:
                   return (
                     <StoneFurnaceDetails
-                      key={i}
+                      key={entity.id}
+                      entity={entity}
+                    />
+                  )
+                case EntityType.enum.BurnerMiningDrill:
+                  return (
+                    <BurnerMiningDrillDetails
+                      key={entity.id}
                       entity={entity}
                     />
                   )
