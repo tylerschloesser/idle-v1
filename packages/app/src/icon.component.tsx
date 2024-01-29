87ca6de277d5ab1d744c99ee34e66eb1769f0fc7
Tyler Schloesser <tylerschloesser@gmail.com>
2024-01-28 18:18:05 -0800

    refactor entity icon

--- packages/app/src/icon.component.tsx
+++ packages/app/src/icon.component.tsx
@@ -1,7 +1,14 @@
-import { faCircle } from '@fortawesome/pro-solid-svg-icons'
+import {
+  faBoreHole,
+  faCircle,
+  faEngine,
+  faFire,
+  faGears,
+  faMicroscope,
+} from '@fortawesome/pro-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { getItemColor } from './color.js'
-import { ItemType } from './world.js'
+import { EntityType, ItemType } from './world.js'
 
 export interface ItemIconProps {
   type: ItemType
@@ -9,4 +16,20 @@ export interface ItemIconProps {
 export function ItemIcon({ type }: ItemIconProps) {
   const color = getItemColor(type)
   return <FontAwesomeIcon icon={faCircle} color={color} />
+}
+
+export function EntityIcon({ type }: { type: EntityType }) {
+  switch (type) {
+    case EntityType.enum.StoneFurnace:
+      return <FontAwesomeIcon icon={faFire} />
+    case EntityType.enum.BurnerMiningDrill:
+      return <FontAwesomeIcon icon={faBoreHole} />
+    case EntityType.enum.Generator:
+      return <FontAwesomeIcon icon={faEngine} />
+    case EntityType.enum.Assembler:
+      return <FontAwesomeIcon icon={faGears} />
+    case EntityType.enum.Lab:
+      return <FontAwesomeIcon icon={faMicroscope} />
+  }
+  return null
 }
--- packages/app/src/world-build.component.tsx
+++ packages/app/src/world-build.component.tsx
@@ -1,39 +1,13 @@
-import {
-  faBoreHole,
-  faCircle,
-  faEngine,
-  faFire,
-  faGears,
-  faMicroscope,
-} from '@fortawesome/pro-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { Fragment, useContext } from 'react'
 import invariant from 'tiny-invariant'
 import { Button } from './button.component.js'
-import { getItemColor } from './color.js'
 import { Context } from './context.js'
 import { Heading3 } from './heading.component.js'
-import { ItemIcon } from './icon.component.js'
+import { EntityIcon, ItemIcon } from './icon.component.js'
 import { canFulfillRecipe } from './inventory.js'
 import { Text } from './text.component.js'
 import styles from './world-build.module.scss'
 import { EntityType, ItemType } from './world.js'
-
-function BuildingIcon({ type }: { type: EntityType }) {
-  switch (type) {
-    case EntityType.enum.StoneFurnace:
-      return <FontAwesomeIcon icon={faFire} />
-    case EntityType.enum.BurnerMiningDrill:
-      return <FontAwesomeIcon icon={faBoreHole} />
-    case EntityType.enum.Generator:
-      return <FontAwesomeIcon icon={faEngine} />
-    case EntityType.enum.Assembler:
-      return <FontAwesomeIcon icon={faGears} />
-    case EntityType.enum.Lab:
-      return <FontAwesomeIcon icon={faMicroscope} />
-  }
-  return null
-}
 
 function BuildEntity({ type }: { type: EntityType }) {
   const { world, buildEntity } = useContext(Context)
@@ -43,7 +17,7 @@ function BuildEntity({ type }: { type: EntityType }) {
   return (
     <>
       <div className={styles['building-name']}>
-        <BuildingIcon type={type} />
+        <EntityIcon type={type} />
         <Text>{type}</Text>
       </div>
       <div className={styles['building-details']}>
