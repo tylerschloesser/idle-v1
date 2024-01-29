7434c3e8e03f950ce1391fab9f6e1ef0ef4b3bff
Tyler Schloesser <tylerschloesser@gmail.com>
2024-01-28 18:01:58 -0800

    add more colors

--- packages/app/src/color.ts
+++ packages/app/src/color.ts
@@ -3,7 +3,13 @@ import { ItemType } from './world.js'
 export function getItemColor(itemType: ItemType): string {
   switch (itemType) {
     case ItemType.enum.CopperOre:
-      return 'hsl(323, 100%, 75%)'
+      return 'hsl(0, 100%, 75%)'
+    case ItemType.enum.IronOre:
+      return 'hsl(180, 25%, 75%)'
+    case ItemType.enum.Stone:
+      return 'hsl(0, 0%, 75%)'
+    case ItemType.enum.Coal:
+      return 'hsl(0, 0%, 25%)'
     default:
       return 'white'
   }
