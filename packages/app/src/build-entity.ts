f78cad6a4115348bf8b2a0dd0b9ed1dc08461111
Tyler Schloesser <tylerschloesser@gmail.com>
2024-01-29 19:27:04 -0800

    refactor build entity

--- packages/app/src/build-entity.ts
+++ packages/app/src/build-entity.ts
@@ -10,78 +10,67 @@ import {
   World,
 } from './world.js'
 
-type BuildEntityFn<T extends Entity> = (
+function buildStoneFurance(
   world: World,
   groupId: GroupId,
-) => T
+): StoneFurnaceEntity {
+  return {
+    type: EntityType.enum.StoneFurnace,
+    id: `${world.nextEntityId++}`,
+    groupId,
+    recipeItemType: null,
+    enabled: false,
+  }
+}
 
-const buildStoneFurance: BuildEntityFn<
-  StoneFurnaceEntity
-> = (world, groupId) => ({
-  type: EntityType.enum.StoneFurnace,
-  id: `${world.nextEntityId++}`,
-  groupId,
-  recipeItemType: null,
-  craftTicksRemaining: null,
-  fuelTicksRemaining: 0,
-  enabled: false,
-})
+function buildBurnerMiningDrill(
+  world: World,
+  groupId: GroupId,
+): BurnerMiningDrillEntity {
+  return {
+    type: EntityType.enum.BurnerMiningDrill,
+    id: `${world.nextEntityId++}`,
+    groupId,
+    enabled: false,
+    resourceType: null,
+  }
+}
 
-const buildBurnerMiningDrill: BuildEntityFn<
-  BurnerMiningDrillEntity
-> = (world, groupId) => ({
-  type: EntityType.enum.BurnerMiningDrill,
-  id: `${world.nextEntityId++}`,
-  groupId,
-  enabled: false,
-  mineTicksRemaining: null,
-  fuelTicksRemaining: 0,
-  resourceType: null,
-})
+function buildGenerator(
+  world: World,
+  groupId: GroupId,
+): GeneratorEntity {
+  return {
+    type: EntityType.enum.Generator,
+    id: `${world.nextEntityId++}`,
+    groupId,
+    enabled: false,
+  }
+}
 
-const buildGenerator: BuildEntityFn<GeneratorEntity> = (
-  world,
-  groupId,
-) => ({
-  type: EntityType.enum.Generator,
-  id: `${world.nextEntityId++}`,
-  groupId,
-  enabled: false,
-  fuelTicksRemaining: 0,
-})
+function buildAssembler(
+  world: World,
+  groupId: GroupId,
+): AssemblerEntity {
+  return {
+    type: EntityType.enum.Assembler,
+    id: `${world.nextEntityId++}`,
+    groupId,
+    enabled: false,
+    recipeItemType: null,
+  }
+}
 
-const buildAssembler: BuildEntityFn<AssemblerEntity> = (
-  world,
-  groupId,
-) => ({
-  type: EntityType.enum.Assembler,
-  id: `${world.nextEntityId++}`,
-  groupId,
-  enabled: false,
-  recipeItemType: null,
-  craftTicksRemaining: null,
-})
-
-const buildLab: BuildEntityFn<LabEntity> = (
-  world,
-  groupId,
-) => ({
-  type: EntityType.enum.Lab,
-  id: `${world.nextEntityId++}`,
-  groupId,
-  enabled: false,
-})
-
-const BUILD_MAP: Record<
-  EntityType,
-  BuildEntityFn<Entity>
-> = {
-  [EntityType.enum.StoneFurnace]: buildStoneFurance,
-  [EntityType.enum.BurnerMiningDrill]:
-    buildBurnerMiningDrill,
-  [EntityType.enum.Generator]: buildGenerator,
-  [EntityType.enum.Assembler]: buildAssembler,
-  [EntityType.enum.Lab]: buildLab,
+function buildLab(
+  world: World,
+  groupId: GroupId,
+): LabEntity {
+  return {
+    type: EntityType.enum.Lab,
+    id: `${world.nextEntityId++}`,
+    groupId,
+    enabled: false,
+  }
 }
 
 export function buildEntity(
@@ -89,5 +78,16 @@ export function buildEntity(
   entityType: EntityType,
   groupId: GroupId,
 ): Entity {
-  return BUILD_MAP[entityType](world, groupId)
+  switch (entityType) {
+    case EntityType.enum.StoneFurnace:
+      return buildStoneFurance(world, groupId)
+    case EntityType.enum.BurnerMiningDrill:
+      return buildBurnerMiningDrill(world, groupId)
+    case EntityType.enum.Generator:
+      return buildGenerator(world, groupId)
+    case EntityType.enum.Assembler:
+      return buildAssembler(world, groupId)
+    case EntityType.enum.Lab:
+      return buildLab(world, groupId)
+  }
 }
