712587e1e71af82490e42ac98b7c74434b6e50cb
Tyler Schloesser <tylerschloesser@gmail.com>
2024-02-01 20:08:11 -0800

    cleanup render

--- packages/app/src/world-map.component.tsx
+++ packages/app/src/world-map.component.tsx
@@ -1,13 +1,6 @@
-import {
-  MutableRefObject,
-  useContext,
-  useEffect,
-  useRef,
-} from 'react'
+import { useContext, useEffect, useRef } from 'react'
 import invariant from 'tiny-invariant'
-import { BoundingBox } from './bounding-box.js'
 import { Context } from './context.js'
-import { Vec2 } from './vec2.js'
 import styles from './world-map.module.scss'
 import { World } from './world.js'
 
@@ -25,7 +18,7 @@ export function WorldMap() {
     const canvas = canvasRef.current
     invariant(canvas)
     initResizeObserver(canvas, controller.signal)
-    initRenderLoop(canvas, controller.signal, worldRef)
+    initRenderLoop(canvas, controller.signal)
     return () => {
       controller.abort()
     }
@@ -47,50 +40,9 @@ function getContext(
   return context
 }
 
-function drawEntityCommon(
-  context: CanvasRenderingContext2D,
-  position: Vec2,
-  size: Vec2,
-  translate: Vec2,
-  scale: number,
-): void {
-  context.fillStyle = 'black'
-  context.strokeStyle = 'white'
-  context.lineWidth = 2
-  context.fillRect(
-    (position.x + translate.x) * scale,
-    (position.y + translate.y) * scale,
-    size.x * scale,
-    size.y * scale,
-  )
-  context.strokeRect(
-    (position.x + translate.x) * scale + 1,
-    (position.y + translate.y) * scale + 1,
-    size.x * scale - 2,
-    size.y * scale - 2,
-  )
-}
-
-function drawEntity(
-  context: CanvasRenderingContext2D,
-  position: Vec2,
-  size: Vec2,
-  translate: Vec2,
-  scale: number,
-): void {
-  drawEntityCommon(
-    context,
-    position,
-    size,
-    translate,
-    scale,
-  )
-}
-
 function initRenderLoop(
   canvas: HTMLCanvasElement,
   signal: AbortSignal,
-  worldRef: MutableRefObject<World>,
 ): void {
   const context = getContext(canvas)
 
@@ -104,51 +56,6 @@ function initRenderLoop(
 
     context.fillStyle = 'hsl(0, 0%, 50%)'
     context.fillRect(0, 0, canvas.width, canvas.height)
-
-    const world = worldRef.current
-
-    const entities = Object.values(world.entities)
-
-    const bb = new BoundingBox()
-    for (let i = 0; i < entities.length; i++) {
-      const position = new Vec2(i * 2, 0)
-      const size = new Vec2(1, 1)
-      bb.add(position, size)
-    }
-
-    // add padding
-    const size = bb.size().add(new Vec2(2, 2))
-    const scale = Math.min(
-      canvas.width / size.x,
-      canvas.height / size.y,
-    )
-
-    let translate: Vec2
-
-    if (size.x > size.y) {
-      translate = new Vec2(
-        0,
-        (canvas.height / scale - size.y) / 2,
-      )
-    } else {
-      translate = new Vec2(
-        (canvas.width / scale - size.x) / 2,
-        0,
-      )
-    }
-
-    // add padding
-    translate = translate.add(new Vec2(1, 1))
-
-    for (let i = 0; i < entities.length; i++) {
-      const entity = entities.at(i)
-      invariant(entity)
-
-      const position = new Vec2(i * 2, 0)
-      const size = new Vec2(1, 1)
-
-      drawEntity(context, position, size, translate, scale)
-    }
 
     window.requestAnimationFrame(render)
   }
