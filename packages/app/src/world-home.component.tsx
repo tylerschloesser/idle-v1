59768c96d99639cbb6a7a169081254774dfc39c8
Tyler Schloesser <tylerschloesser@gmail.com>
2024-02-05 12:36:53 -0800

    add entity card

--- packages/app/src/world-home.component.tsx
+++ packages/app/src/world-home.component.tsx
@@ -1,6 +1,30 @@
 import { Heading2 } from './heading.component.js'
 import { useModel } from './world-home.hooks.js'
 import styles from './world-home.module.scss'
+import {
+  Entity,
+  EntityType,
+  HandMinerEntity,
+} from './world.js'
+
+type HandMinerEntityCardProps = {
+  entity: HandMinerEntity
+}
+
+function HandMinerEntityCard({
+  entity,
+}: HandMinerEntityCardProps) {
+  return <>{entity.type}</>
+}
+
+function renderEntityCard(entity: Entity) {
+  switch (entity.type) {
+    case EntityType.enum.HandMiner:
+      return <HandMinerEntityCard entity={entity} />
+    default:
+      return <>TODO {entity.type}</>
+  }
+}
 
 export function WorldHome() {
   const model = useModel()
@@ -12,7 +36,7 @@ export function WorldHome() {
       <Heading2>Entities</Heading2>
       {model.entities.map((entity) => (
         <div className={styles.entity} key={entity.id}>
-          {entity.type}
+          {renderEntityCard(entity)}
         </div>
       ))}
     </div>
