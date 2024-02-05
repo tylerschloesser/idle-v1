b9847de84374f078983fce3136711f7ebaa341c3
Tyler Schloesser <tylerschloesser@gmail.com>
2024-02-05 12:20:14 -0800

    add group id

--- packages/app/src/world-home.component.tsx
+++ packages/app/src/world-home.component.tsx
@@ -3,7 +3,21 @@ import { useSearchParams } from 'react-router-dom'
 import invariant from 'tiny-invariant'
 import { Context } from './context.js'
 import './world.js'
-import { BlockId } from './world.js'
+import { BlockId, GroupId } from './world.js'
+
+export function WorldHome() {
+  const blockId = useBlockId()
+  const groupId = useGroupId(blockId)
+
+  if (!blockId || !groupId) {
+    return null
+  }
+  return (
+    <>
+      {blockId}-{groupId}
+    </>
+  )
+}
 
 function useBlockId(): BlockId | null {
   const { world } = useContext(Context)
@@ -24,11 +38,33 @@ function useBlockId(): BlockId | null {
   return blockId ? BlockId.parse(blockId) : null
 }
 
-export function WorldHome() {
-  const blockId = useBlockId()
+function useGroupId(
+  blockId: BlockId | null,
+): GroupId | null {
+  const { world } = useContext(Context)
+  const [search, setSearch] = useSearchParams()
+  const groupId = search.get('groupId')
 
-  if (!blockId) {
-    return null
-  }
-  return <>{blockId}</>
+  useEffect(() => {
+    const block = blockId
+      ? world.blocks?.[blockId]
+      : undefined
+    if (block === undefined) {
+      // should be updated in the block effect
+      return
+    }
+
+    if (groupId === null || !block.groupIds[groupId]) {
+      const defaultGroupId = Object.keys(block.groupIds).at(
+        0,
+      )
+      invariant(defaultGroupId)
+      setSearch((next) => {
+        next.set('groupId', defaultGroupId)
+        return next
+      })
+    }
+  }, [blockId, groupId])
+
+  return groupId ? GroupId.parse(groupId) : null
 }
