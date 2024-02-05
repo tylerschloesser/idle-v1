6fc45371a2989010c7062ca7b6d11406f7737794
Tyler Schloesser <tylerschloesser@gmail.com>
2024-02-05 12:14:53 -0800

    set block id

--- packages/app/src/world-home.component.tsx
+++ packages/app/src/world-home.component.tsx
@@ -1,9 +1,34 @@
-import { useContext } from 'react'
+import { useContext, useEffect } from 'react'
+import { useSearchParams } from 'react-router-dom'
+import invariant from 'tiny-invariant'
 import { Context } from './context.js'
 import './world.js'
+import { BlockId } from './world.js'
+
+function useBlockId(): BlockId | null {
+  const { world } = useContext(Context)
+  const [search, setSearch] = useSearchParams()
+  const blockId = search.get('blockId')
+
+  useEffect(() => {
+    if (blockId === null || !world.blocks[blockId]) {
+      const defaultBlockId = Object.keys(world.blocks).at(0)
+      invariant(defaultBlockId)
+      setSearch((next) => {
+        next.set('blockId', defaultBlockId)
+        return next
+      })
+    }
+  }, [blockId])
+
+  return blockId ? BlockId.parse(blockId) : null
+}
 
 export function WorldHome() {
-  const { world } = useContext(Context)
+  const blockId = useBlockId()
 
-  return <>TODO</>
+  if (!blockId) {
+    return null
+  }
+  return <>{blockId}</>
 }
