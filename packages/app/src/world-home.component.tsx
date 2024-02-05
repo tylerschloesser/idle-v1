dc87a5e5da11fc4a62fdb3b2725c686d44cc937c
Tyler Schloesser <tylerschloesser@gmail.com>
2024-02-05 12:28:08 -0800

    update heading

--- packages/app/src/world-home.component.tsx
+++ packages/app/src/world-home.component.tsx
@@ -1,4 +1,6 @@
+import { Heading2 } from './heading.component.js'
 import { useModel } from './world-home.hooks.js'
+import styles from './world-home.module.scss'
 
 export function WorldHome() {
   const model = useModel()
@@ -6,10 +8,11 @@ export function WorldHome() {
   if (!model) return null
 
   return (
-    <>
+    <div className={styles['world-home']}>
+      <Heading2>Entities</Heading2>
       {model.entities.map((entity) => (
         <div key={entity.id}>{entity.type}</div>
       ))}
-    </>
+    </div>
   )
 }
