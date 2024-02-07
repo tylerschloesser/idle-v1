0537428b299af11e23fc7f87c15e6801407ef192
Tyler Schloesser <tylerschloesser@gmail.com>
2024-02-06 17:48:16 -0800

    switch storybook to typescript

--- packages/app/.storybook/main.ts
+++ packages/app/.storybook/main.ts
@@ -1,17 +1,17 @@
+import type { StorybookConfig } from '@storybook/react-webpack5'
 import { join, dirname } from 'path'
 
 /**
  * This function is used to resolve the absolute path of a package.
  * It is needed in projects that use Yarn PnP or are set up within a monorepo.
  */
-function getAbsolutePath(value) {
+function getAbsolutePath(value: string): string {
   return dirname(
     require.resolve(join(value, 'package.json')),
   )
 }
 
-/** @type { import('@storybook/react-webpack5').StorybookConfig } */
-const config = {
+const config: StorybookConfig = {
   stories: [
     '../src/**/*.mdx',
     '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)',
@@ -23,7 +23,7 @@ const config = {
     getAbsolutePath('@storybook/addon-interactions'),
   ],
   framework: {
-    name: getAbsolutePath('@storybook/react-webpack5'),
+    name: '@storybook/react-webpack5',
     options: {
       builder: {
         useSWC: true,
