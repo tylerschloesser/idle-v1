c26620831769c4b400074942901f4ac0f5f6cf4b
Tyler Schloesser <tylerschloesser@gmail.com>
2024-02-08 14:58:25 -0800

    refactor tick hand miner

---
+++ packages/app/src/tick-hand-miner.ts
@@ -0,0 +1,43 @@
+import invariant from 'tiny-invariant'
+import {
+  HAND_MINE_PRODUCTION_PER_TICK,
+  HAND_MINE_TICK_COUNT,
+} from './const.js'
+import { outputToEntity } from './tick-util.js'
+import { HandMinerEntity, World } from './world.js'
+
+export function tickHandMiner(
+  world: World,
+  entity: HandMinerEntity,
+): void {
+  const head = entity.queue.at(0)
+  if (!head) {
+    return
+  }
+
+  const targetTicks = head.count * HAND_MINE_TICK_COUNT
+  invariant(head.ticks < targetTicks)
+
+  invariant(Object.keys(entity.output).length === 1)
+
+  const outputEntityId = Object.keys(entity.output).at(0)
+  invariant(outputEntityId)
+
+  const output = world.entities[outputEntityId]
+  invariant(output)
+
+  outputToEntity(
+    {
+      [head.resourceType]: {
+        count: HAND_MINE_PRODUCTION_PER_TICK,
+        condition: 1,
+      },
+    },
+    output,
+  )
+
+  head.ticks += 1
+  if (head.ticks >= targetTicks) {
+    entity.queue.shift()
+  }
+}
--- packages/app/src/tick-world.ts
+++ packages/app/src/tick-world.ts
@@ -1,50 +1,5 @@
-import invariant from 'tiny-invariant'
-import {
-  HAND_MINE_PRODUCTION_PER_TICK,
-  HAND_MINE_TICK_COUNT,
-} from './const.js'
-import { outputToEntity } from './tick-util.js'
-import {
-  EntityType,
-  HandMinerEntity,
-  World,
-} from './world.js'
-
-function tickHandMiner(
-  world: World,
-  entity: HandMinerEntity,
-): void {
-  const head = entity.queue.at(0)
-  if (!head) {
-    return
-  }
-
-  const targetTicks = head.count * HAND_MINE_TICK_COUNT
-  invariant(head.ticks < targetTicks)
-
-  invariant(Object.keys(entity.output).length === 1)
-
-  const outputEntityId = Object.keys(entity.output).at(0)
-  invariant(outputEntityId)
-
-  const output = world.entities[outputEntityId]
-  invariant(output)
-
-  outputToEntity(
-    {
-      [head.resourceType]: {
-        count: HAND_MINE_PRODUCTION_PER_TICK,
-        condition: 1,
-      },
-    },
-    output,
-  )
-
-  head.ticks += 1
-  if (head.ticks >= targetTicks) {
-    entity.queue.shift()
-  }
-}
+import { tickHandMiner } from './tick-hand-miner.js'
+import { EntityType, World } from './world.js'
 
 export function tickWorld(world: World): void {
   for (const entity of Object.values(world.entities)) {
