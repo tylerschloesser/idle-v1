ee05a2e9f80b6b0d6f737d53e8cd7e2923a31b37
Tyler Schloesser <tylerschloesser@gmail.com>
2024-02-09 11:08:06 -0800

    show available

--- packages/app/src/components/build-view.tsx
+++ packages/app/src/components/build-view.tsx
@@ -1,32 +1,43 @@
+import { Heading2 } from '../heading.component.js'
 import { ITEM_TYPE_TO_LABEL } from '../item-label.component.js'
 import { Entity } from '../world.js'
+import styles from './build-view.module.scss'
+
+export type ActiveEntity = Pick<
+  Entity,
+  'id' | 'type' | 'scale'
+> & {
+  available: number
+}
 
 export interface BuildViewProps {
-  entities: Entity[]
+  entities: ActiveEntity[]
 }
 
 export function BuildView({ entities }: BuildViewProps) {
   return (
-    <>
+    <div className={styles['build-view']}>
+      <Heading2>Active</Heading2>
       {entities.map((entity) => (
         <ExistingEntityCard
           key={entity.id}
           entity={entity}
         />
       ))}
-    </>
+    </div>
   )
 }
 
 function ExistingEntityCard({
   entity,
 }: {
-  entity: Entity
+  entity: ActiveEntity
 }) {
   return (
-    <>
+    <div className={styles['existing-entity-card']}>
       <div>{ITEM_TYPE_TO_LABEL[entity.type]} #1</div>
-      <div>{entity.scale}</div>
-    </>
+      <div>scale: {entity.scale}</div>
+      <div>available: {entity.available}</div>
+    </div>
   )
 }
--- packages/app/src/pages/build-page.tsx
+++ packages/app/src/pages/build-page.tsx
@@ -1,21 +1,52 @@
 import { useContext } from 'react'
 import invariant from 'tiny-invariant'
-import { BuildView } from '../components/build-view.js'
+import {
+  ActiveEntity,
+  BuildView,
+} from '../components/build-view.js'
 import { GroupContext, useWorld } from '../context.js'
-import { Entity } from '../world.js'
+import { EntityType, ItemType } from '../world.js'
 
 export function BuildPage() {
-  const entities = useEntities()
+  const entities = useActiveEntities()
   return <BuildView entities={entities} />
 }
 
-function useEntities(): Entity[] {
+function useActiveEntities(): ActiveEntity[] {
   const world = useWorld()
   const { group } = useContext(GroupContext)
+
+  const available: Partial<Record<EntityType, number>> = {}
+  for (const entityId of Object.keys(group.entityIds)) {
+    const entity = world.entities[entityId]
+    invariant(entity)
+
+    if (entity.type !== EntityType.enum.Buffer) continue
+
+    for (const [key, value] of Object.entries(
+      entity.contents,
+    )) {
+      const itemType = ItemType.parse(key)
+      if (isEntityType(itemType) && value.count >= 1) {
+        available[itemType] = (available[itemType] ?? 0) + 1
+      }
+    }
+  }
 
   return Object.keys(group.entityIds).map((entityId) => {
     const entity = world.entities[entityId]
     invariant(entity)
-    return entity
+    return {
+      id: entity.id,
+      type: entity.type,
+      scale: entity.scale,
+      available: available[entity.type] ?? 0,
+    }
   })
+}
+
+function isEntityType(
+  itemType: ItemType,
+): itemType is EntityType {
+  return EntityType.safeParse(itemType).success
 }
