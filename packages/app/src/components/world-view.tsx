8325196f25ed5cff469c8cd8d586e32488cf2622
Tyler Schloesser <tylerschloesser@gmail.com>
2024-02-09 10:11:56 -0800

    fix world view

--- packages/app/src/components/world-view.tsx
+++ packages/app/src/components/world-view.tsx
@@ -1,10 +1,13 @@
 import { Outlet } from 'react-router-dom'
 import { TabBar } from '../tab-bar.component.js'
+import styles from './world-view.module.scss'
 
 export function WorldView() {
   return (
     <>
-      <Outlet />
+      <div className={styles['world-view']}>
+        <Outlet />
+      </div>
       <TabBar />
     </>
   )
