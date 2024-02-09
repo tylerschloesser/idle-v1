8938a7757e218e9798e24c7d7bfa45a93c56f360
Tyler Schloesser <tylerschloesser@gmail.com>
2024-02-09 10:53:12 -0800

    add existing entity card

--- packages/app/src/pages/build-page.tsx
+++ packages/app/src/pages/build-page.tsx
@@ -4,15 +4,29 @@ import { GroupContext, useWorld } from '../context.js'
 import { ITEM_TYPE_TO_LABEL } from '../item-label.component.js'
 import { Entity } from '../world.js'
 
+function ExistingEntityCard({
+  entity,
+}: {
+  entity: Entity
+}) {
+  return (
+    <>
+      <div>{ITEM_TYPE_TO_LABEL[entity.type]} #1</div>
+      <div>{entity.scale}</div>
+    </>
+  )
+}
+
 export function BuildPage() {
   const entities = useEntities()
 
   return (
     <>
       {entities.map((entity) => (
-        <div key={entity.id}>
-          {ITEM_TYPE_TO_LABEL[entity.type]} #1
-        </div>
+        <ExistingEntityCard
+          key={entity.id}
+          entity={entity}
+        />
       ))}
     </>
   )
