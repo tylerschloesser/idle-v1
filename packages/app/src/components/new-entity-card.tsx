16f9f053991b09842f194f1a1c7b54503e910fc4
Tyler Schloesser <tylerschloesser@gmail.com>
2024-02-10 18:10:17 -0800

    render options

--- packages/app/src/components/new-entity-card.tsx
+++ packages/app/src/components/new-entity-card.tsx
@@ -1,3 +1,10 @@
+import { Button } from '../button.component.js'
+import { ItemIcon } from '../icon.component.js'
+import {
+  ITEM_TYPE_TO_LABEL,
+  ItemLabel,
+} from '../item-label.component.js'
+import { Text } from '../text.component.js'
 import { EntityType } from '../world.js'
 import styles from './new-entity-card.module.scss'
 
@@ -10,14 +17,26 @@ export function NewEntityCard({
 }: NewEntityCardProps) {
   return (
     <div className={styles['new-entity-card']}>
-      {mapAvailableEntityTypes(
-        availableEntityTypes,
-        (entityType, count) => (
-          <div key={entityType}>
-            {entityType}: {count}
-          </div>
-        ),
-      )}
+      <div className={styles['entity-option-group']}>
+        {mapAvailableEntityTypes(
+          availableEntityTypes,
+          (entityType, count) => (
+            <div
+              className={styles['entity-option']}
+              key={entityType}
+            >
+              <div>
+                <ItemIcon type={entityType} />
+                <Text>
+                  {ITEM_TYPE_TO_LABEL[entityType]}
+                </Text>
+              </div>
+              <Text>{count} Available</Text>
+            </div>
+          ),
+        )}
+      </div>
+      <Button onClick={() => {}}>Build</Button>
     </div>
   )
 }
