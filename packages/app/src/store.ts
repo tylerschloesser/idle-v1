514622531029e932f77df68fd3dd33820e56a178
Tyler Schloesser <tylerschloesser@gmail.com>
2024-02-14 15:46:49 -0800

    attempt async thunk

--- packages/app/src/store.ts
+++ packages/app/src/store.ts
@@ -1,10 +1,12 @@
 import {
   configureStore,
   createAction,
+  createAsyncThunk,
   createReducer,
 } from '@reduxjs/toolkit'
 import invariant from 'tiny-invariant'
-import { fastForward } from './world-api.js'
+import { TICK_RATE } from './const.js'
+import { tickWorld } from './tick-world.js'
 import {
   EntityId,
   EntityType,
@@ -12,11 +14,35 @@ import {
   World,
 } from './world.js'
 
-export const appVisible = createAction('app-visible')
-export const appHidden = createAction('app-hidden')
-
 export const tick = createAction<{ tickCount: number }>(
   'tick',
+)
+// prettier-ignore
+export type RootState = {
+  tickIntervalId: number | null
+  world: World
+}
+
+export const appVisible = createAsyncThunk<null | number>(
+  'app-visible',
+  async (_, thunkApi) => {
+    const state = thunkApi.getState() as RootState
+
+    if (state.tickIntervalId !== null) {
+      return null
+    }
+    return self.setInterval(() => {
+      thunkApi.dispatch(tick({ tickCount: 1 }))
+    }, TICK_RATE)
+  },
+)
+
+export const appHidden = createAsyncThunk(
+  'app-hidden',
+  async (_, thunkApi) => {
+    const state = thunkApi.getState() as RootState
+    self.clearInterval(state.tickIntervalId ?? undefined)
+  },
 )
 
 export const enqueueHandMineOperation = createAction<{
@@ -25,45 +51,54 @@ export const enqueueHandMineOperation = createAction<{
 }>('enqueue-hand-mine-operation')
 
 export const createStore = (world: World) =>
-  configureStore({
-    reducer: createReducer(world, (builder) => {
-      builder.addCase(
-        enqueueHandMineOperation,
-        (world, action) => {
-          const { entityId, resourceType } = action.payload
+  configureStore<RootState>({
+    reducer: createReducer(
+      { tickIntervalId: null as number | null, world },
+      (builder) => {
+        builder.addCase(
+          enqueueHandMineOperation,
+          ({ world }, action) => {
+            const { entityId, resourceType } =
+              action.payload
 
-          const entity = world.entities[entityId]
-          invariant(
-            entity?.type === EntityType.enum.HandMiner,
-          )
+            const entity = world.entities[entityId]
+            invariant(
+              entity?.type === EntityType.enum.HandMiner,
+            )
 
-          const tail = entity.queue.at(-1)
-          if (tail?.resourceType === resourceType) {
-            tail.count += 1
-          } else {
-            entity.queue.push({
-              id: self.crypto.randomUUID(),
-              count: 1,
-              resourceType,
-              ticks: 0,
-            })
-          }
-        },
-      )
+            const tail = entity.queue.at(-1)
+            if (tail?.resourceType === resourceType) {
+              tail.count += 1
+            } else {
+              entity.queue.push({
+                id: self.crypto.randomUUID(),
+                count: 1,
+                resourceType,
+                ticks: 0,
+              })
+            }
+          },
+        )
 
-      builder.addCase(tick, (world, action) => {
-        const { tickCount } = action.payload
-        console.log('todo tick world')
-      })
-      builder.addCase(appVisible, (world) => {
-        fastForward(world)
-        // TODO start ticker
-      })
-      builder.addCase(appHidden, (world) => {})
-    }),
+        builder.addCase(tick, (state) => {
+          tickWorld(state.world)
+        })
+
+        builder.addCase(
+          appVisible.fulfilled,
+          (state, action) => {
+            if (action.payload !== null) {
+              state.tickIntervalId = action.payload
+            }
+          },
+        )
+
+        builder.addCase(appHidden.fulfilled, (state) => {
+          state.tickIntervalId = null
+        })
+      },
+    ),
   })
 
-// prettier-ignore
-export type RootState = ReturnType<ReturnType<typeof createStore>['getState']>
 // prettier-ignore
 export type AppDispatch = ReturnType<typeof createStore >['dispatch']
