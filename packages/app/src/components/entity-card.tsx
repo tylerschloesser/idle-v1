1e1c218c630f19cb8fdd1519535df3f673b67965
Tyler Schloesser <tylerschloesser@gmail.com>
2024-02-14 13:54:40 -0800

    fix entity card animation

--- packages/app/src/components/entity-card.tsx
+++ packages/app/src/components/entity-card.tsx
@@ -26,54 +26,57 @@ export function EntityCard({
   const visible =
     entity.cardState !== EntityCardState.enum.Hidden
   return (
-    <motion.div
-      layout
-      className={styles['card']}
-      style={{ borderWidth: 2 }}
-    >
-      <motion.div layout className={styles['card-header']}>
-        <span>
-          <ItemIcon type={entity.type} />{' '}
-          <Text bold>
-            {ITEM_TYPE_TO_LABEL[entity.type]} {'#1'}
-          </Text>
-          <Text gray>{` [${entity.scale}]`}</Text>
-        </span>
-        <div className={styles['toggle-group']}>
-          <button className={styles.toggle}>
-            <FontAwesomeIcon icon={faGear} fixedWidth />
-          </button>
-          <button
-            className={styles.toggle}
-            onClick={() => {
-              setEntityCardState(
-                entity.id,
-                visible
-                  ? EntityCardState.enum.Hidden
-                  : EntityCardState.enum.Visible,
-              )
-            }}
-          >
-            <FontAwesomeIcon
-              icon={visible ? faMinus : faPlus}
-              fixedWidth
-            />
-          </button>
+    <motion.div layout className={styles['card']}>
+      <motion.div
+        layout="position"
+        className={styles['card-inner']}
+      >
+        <div className={styles['card-header']}>
+          <span>
+            <ItemIcon type={entity.type} />{' '}
+            <Text bold>
+              {ITEM_TYPE_TO_LABEL[entity.type]} {'#1'}
+            </Text>
+            <Text gray>{` [${entity.scale}]`}</Text>
+          </span>
+          <div className={styles['toggle-group']}>
+            <button className={styles.toggle}>
+              <FontAwesomeIcon icon={faGear} fixedWidth />
+            </button>
+            <button
+              className={styles.toggle}
+              onClick={() => {
+                setEntityCardState(
+                  entity.id,
+                  visible
+                    ? EntityCardState.enum.Hidden
+                    : EntityCardState.enum.Visible,
+                )
+              }}
+            >
+              <FontAwesomeIcon
+                icon={visible ? faMinus : faPlus}
+                fixedWidth
+              />
+            </button>
+          </div>
         </div>
+        <AnimatePresence initial={false} mode="popLayout">
+          {visible && (
+            <motion.div
+              key={`${entity.id}.content`}
+              className={styles['card-content']}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+            >
+              <div className={styles['card-content-inner']}>
+                {children}
+              </div>
+            </motion.div>
+          )}
+        </AnimatePresence>
       </motion.div>
-      <AnimatePresence>
-        {visible && (
-          <motion.div
-            key={`${entity.id}.content`}
-            layout
-            className={styles['card-content']}
-          >
-            <div className={styles['card-content-inner']}>
-              {children}
-            </div>
-          </motion.div>
-        )}
-      </AnimatePresence>
     </motion.div>
   )
 }
