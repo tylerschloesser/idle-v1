13c6fc649b687c7af7be3b0749214ef1f87b4be3
Tyler Schloesser <tylerschloesser@gmail.com>
2024-02-14 14:30:53 -0800

    add redux

--- packages/app/src/index.tsx
+++ packages/app/src/index.tsx
@@ -1,13 +1,19 @@
 import { config as faConfig } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
 import { createRoot } from 'react-dom/client'
+import { Provider } from 'react-redux'
 import invariant from 'tiny-invariant'
 import { App } from './app.component.js'
 import './index.scss'
+import { store } from './store.js'
 
 faConfig.autoAddCss = false
 
 const container = document.getElementById('root')
 invariant(container)
 
-createRoot(container).render(<App />)
+createRoot(container).render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+)
