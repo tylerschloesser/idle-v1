2fdb32cef9798bd24a13ff5a5134f366e19fd71a
Tyler Schloesser <tylerschloesser@gmail.com>
2024-02-15 09:47:48 -0800

    select buffers

--- packages/app/src/components/new-entity-card-list.tsx
+++ packages/app/src/components/new-entity-card-list.tsx
@@ -1,3 +1,14 @@
+import { useContext } from 'react'
+import { useSelector } from 'react-redux'
+import { GroupContext } from '../context.js'
+import { selectBuffers } from '../selectors.js'
+import { RootState } from '../store.js'
+
 export function NewEntityCardList() {
-  return <>TODO</>
+  const { groupId } = useContext(GroupContext)
+  const buffers = useSelector((state: RootState) =>
+    selectBuffers(state, groupId),
+  )
+
+  return <>{buffers.length}</>
 }
