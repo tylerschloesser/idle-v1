d60075277da218efc842c9d10da67daa7f909f77
Tyler Schloesser <tylerschloesser@gmail.com>
2024-02-15 10:08:05 -0800

    map available

--- packages/app/src/components/new-entity-card-list.tsx
+++ packages/app/src/components/new-entity-card-list.tsx
@@ -1,10 +1,29 @@
-import { useContext } from 'react'
+import { Fragment, useContext } from 'react'
 import { GroupContext } from '../context.js'
 import { useAvailableEntityTypes } from '../hook.js'
+import { EntityType } from '../world.js'
+
+function mapAvailable(
+  available: ReturnType<typeof useAvailableEntityTypes>,
+  cb: (
+    entityType: EntityType,
+    count: number,
+  ) => JSX.Element,
+): JSX.Element[] {
+  return Object.entries(available).map(([key, value]) => (
+    <Fragment key={key}>
+      {cb(EntityType.parse(key), value)}
+    </Fragment>
+  ))
+}
 
 export function NewEntityCardList() {
   const { groupId } = useContext(GroupContext)
   const available = useAvailableEntityTypes(groupId)
-  console.log('render')
-  return <>{JSON.stringify(available)}</>
+
+  return mapAvailable(available, (entityType, count) => (
+    <>
+      TODO {entityType}: {count}
+    </>
+  ))
 }
