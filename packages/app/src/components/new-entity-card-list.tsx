c05ce4a3e83759c4ea739ab9498c2be45f6fea10
Tyler Schloesser <tylerschloesser@gmail.com>
2024-02-15 09:54:49 -0800

    refactor use available

--- packages/app/src/components/new-entity-card-list.tsx
+++ packages/app/src/components/new-entity-card-list.tsx
@@ -1,32 +1,7 @@
-import { useContext } from 'react'
-import { useSelector } from 'react-redux'
-import { GroupContext } from '../context.js'
-import { selectBuffers } from '../selectors.js'
-import { RootState } from '../store.js'
-import {
-  isEntityType,
-  iterateBufferContents,
-} from '../util.js'
-import { EntityType } from '../world.js'
+import { useAvailable } from '../hook.js'
 
 export function NewEntityCardList() {
-  const { groupId } = useContext(GroupContext)
-  const buffers = useSelector((state: RootState) =>
-    selectBuffers(state, groupId),
-  )
-
-  const available: Partial<Record<EntityType, number>> = {}
-  for (const buffer of buffers) {
-    for (const [itemType, count] of iterateBufferContents(
-      buffer,
-    )) {
-      if (!isEntityType(itemType) || count < 1) {
-        continue
-      }
-      available[itemType] =
-        (available[itemType] ?? 0) + Math.floor(count)
-    }
-  }
-
+  const available = useAvailable()
+  console.log('render')
   return <>{JSON.stringify(available)}</>
 }
---
+++ packages/app/src/hook.ts
@@ -0,0 +1,33 @@
+import { useContext } from 'react'
+import { useSelector } from 'react-redux'
+import { GroupContext } from './context.js'
+import { selectBuffers } from './selectors.js'
+import { RootState } from './store.js'
+import {
+  isEntityType,
+  iterateBufferContents,
+} from './util.js'
+import { EntityType } from './world.js'
+
+export function useAvailable(): Partial<
+  Record<EntityType, number>
+> {
+  const { groupId } = useContext(GroupContext)
+  const buffers = useSelector((state: RootState) =>
+    selectBuffers(state, groupId),
+  )
+
+  const available: Partial<Record<EntityType, number>> = {}
+  for (const buffer of buffers) {
+    for (const [itemType, count] of iterateBufferContents(
+      buffer,
+    )) {
+      if (!isEntityType(itemType) || count < 1) {
+        continue
+      }
+      available[itemType] =
+        (available[itemType] ?? 0) + Math.floor(count)
+    }
+  }
+  return available
+}
