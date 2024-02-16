03aa7729efc4013289f6b6ab27f5594bed773754
Tyler Schloesser <tylerschloesser@gmail.com>
2024-02-15 20:18:05 -0800

    add resource type

--- packages/app/src/components/combustion-miner.tsx
+++ packages/app/src/components/combustion-miner.tsx
@@ -1,3 +1,9 @@
+import { Fragment } from 'react'
+import { ItemIcon } from '../icon.component.js'
+import { ITEM_TYPE_TO_LABEL } from '../item-label.component.js'
+import { Text } from '../text.component.js'
+import { ResourceType } from '../world.js'
+import styles from './combustion-miner.module.scss'
 import { ModifyScale } from './modify-scale.js'
 
 export interface EditCombustionMinerProps {
@@ -12,10 +18,35 @@ export function EditCombustionMiner({
   decrementScale,
 }: EditCombustionMinerProps) {
   return (
-    <ModifyScale
-      scale={scale}
-      increment={incrementScale}
-      decrement={decrementScale}
-    />
+    <>
+      <Text>Resource</Text>
+      <div className={styles['resource-option-group']}>
+        {mapResourceTypes((resourceType) => (
+          <Fragment key={resourceType}>
+            <div className={styles['resource-option']}>
+              <ItemIcon type={resourceType} />
+              <Text>
+                {ITEM_TYPE_TO_LABEL[resourceType]}
+              </Text>
+            </div>
+          </Fragment>
+        ))}
+      </div>
+      <Text>Scale</Text>
+      <ModifyScale
+        scale={scale}
+        increment={incrementScale}
+        decrement={decrementScale}
+      />
+    </>
   )
+}
+
+function mapResourceTypes(
+  cb: (resourceType: ResourceType) => JSX.Element,
+): JSX.Element[] {
+  return Object.values(ResourceType.Values).map((value) => {
+    const resourceType = ResourceType.parse(value)
+    return cb(resourceType)
+  })
 }
