4746f9681babc4e4c484479d258ef509d3c80262
Tyler Schloesser <tylerschloesser@gmail.com>
2024-02-18 18:39:32 -0600

    disable eslint

--- packages/app/src/tick-generator.ts
+++ packages/app/src/tick-generator.ts
@@ -1,5 +1,7 @@
 import { GeneratorEntity, World } from './world.js'
 
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
 export function tickGenerator(
   world: World,
   entity: GeneratorEntity,
