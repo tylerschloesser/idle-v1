fb19d47460781ddecf5a7c59fc27732dbf0abe65
Tyler Schloesser <tylerschloesser@gmail.com>
2024-02-22 15:40:44 -0800

    cleanup selectors

--- packages/app/src/selectors.ts
+++ packages/app/src/selectors.ts
@@ -1,8 +1,7 @@
 import { createSelector } from '@reduxjs/toolkit'
 import invariant from 'tiny-invariant'
 import { RootState } from './store.js'
-import { isBuffer, isInGroup } from './util.js'
-import { EntityId, GroupId } from './world.js'
+import { EntityId } from './world.js'
 
 export const selectEntity = createSelector(
   [
@@ -13,21 +12,5 @@ export const selectEntity = createSelector(
     const entity = entities[entityId]
     invariant(entity)
     return entity
-  },
-)
-
-export const selectBuffers = createSelector(
-  [
-    (state: RootState) => state.world.entities,
-    (state: RootState, groupId: GroupId) => {
-      const group = state.world.groups[groupId]
-      invariant(group)
-      return group
-    },
-  ],
-  (entities, group) => {
-    return Object.values(entities)
-      .filter(isInGroup(group))
-      .filter(isBuffer)
   },
 )
