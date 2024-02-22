ec99a9e6fb23e352bb09599873585ee257d2ff4f
Tyler Schloesser <tylerschloesser@gmail.com>
2024-02-22 15:25:40 -0800

    fix block view

--- packages/app/src/components/block-view.tsx
+++ packages/app/src/components/block-view.tsx
@@ -1,33 +1,13 @@
-import { createSelector } from '@reduxjs/toolkit'
 import { LayoutGroup, motion } from 'framer-motion'
 import { useContext } from 'react'
-import { useSelector } from 'react-redux'
-import invariant from 'tiny-invariant'
 import { BlockContext } from '../context.js'
 import { Heading2 } from '../heading.component.js'
-import { RootState } from '../store.js'
-import { BlockId } from '../world.js'
 import styles from './block-view.module.scss'
 import { EntityCard } from './entity-card.js'
 import { NewEntityCardList } from './new-entity-card-list.js'
 
-const selectBlock = createSelector(
-  [
-    (state: RootState) => state.world.blocks,
-    (_state: RootState, blockId: BlockId) => blockId,
-  ],
-  (blocks, blockId) => {
-    const block = blocks[blockId]
-    invariant(block)
-    return block
-  },
-)
-
 export function BlockView() {
-  const { blockId } = useContext(BlockContext)
-  const block = useSelector((state: RootState) =>
-    selectBlock(state, blockId),
-  )
+  const { block } = useContext(BlockContext)
   const entityIds = Object.keys(block.entityIds)
 
   return (
