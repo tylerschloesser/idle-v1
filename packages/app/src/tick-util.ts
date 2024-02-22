56f60425965284c80a3ca615ad6835de296a34fd
Tyler Schloesser <tylerschloesser@gmail.com>
2024-02-22 15:47:32 -0800

    cleanup tick util

--- packages/app/src/tick-util.ts
+++ packages/app/src/tick-util.ts
@@ -1,79 +1,13 @@
 import invariant from 'tiny-invariant'
 import { COMBUSTION_SMELTER_COAL_PER_TICK } from './const.js'
-import { gte } from './util.js'
 import {
   AssemblerRecipe,
-  BufferEntity,
   CombustionSmelterEntity,
-  ConsumeItemTickMetric,
   Entity,
-  EntityType,
   ItemType,
-  ProduceItemTickMetric,
   SmelterRecipe,
-  TickMetricType,
   World,
 } from './world.js'
-
-export function outputToEntity(
-  frame: Partial<
-    Record<
-      ItemType,
-      {
-        count: number
-        condition: number
-      }
-    >
-  >,
-  target: Entity,
-): void {
-  invariant(target.type === EntityType.enum.Buffer)
-
-  for (const [
-    itemType,
-    { count, condition },
-  ] of Object.entries(frame)) {
-    const key = ItemType.parse(itemType)
-
-    let value = target.contents[key]
-    if (!value) {
-      value = target.contents[key] = {
-        count: 0,
-        condition: 1,
-      }
-    }
-
-    // TODO
-    invariant(condition === 1)
-    invariant(value.condition === 1)
-
-    value.count += count
-  }
-}
-
-export function getInputBuffer(
-  world: World,
-  entity: Entity,
-): BufferEntity {
-  const entityIds = Object.keys(entity.input)
-  invariant(entityIds.length === 1)
-  const entityId = entityIds.at(0)!
-  const buffer = world.entities[entityId]
-  invariant(buffer?.type === EntityType.enum.Buffer)
-  return buffer
-}
-
-export function getOutputBuffer(
-  world: World,
-  entity: Entity,
-): BufferEntity {
-  const entityIds = Object.keys(entity.output)
-  invariant(entityIds.length === 1)
-  const entityId = entityIds.at(0)!
-  const buffer = world.entities[entityId]
-  invariant(buffer?.type === EntityType.enum.Buffer)
-  return buffer
-}
 
 export function* iterateRecipeInput({
   recipe,
@@ -149,65 +83,6 @@ export function* iterateRecipeOutput({
         scale *
         satisfaction,
     ]
-  }
-}
-
-export function consume({
-  entity,
-  input,
-  itemType,
-  count,
-}: {
-  entity?: Entity
-  input: BufferEntity
-  itemType: ItemType
-  count: number
-}): void {
-  const value = input.contents[itemType]
-  invariant(value)
-  invariant(gte(value.count, count))
-
-  value.count = Math.max(value.count - count, 0)
-
-  if (entity) {
-    const metric: ConsumeItemTickMetric = {
-      type: TickMetricType.enum.ConsumeItem,
-      sourceEntityId: input.id,
-      itemType,
-      count,
-    }
-    entity.metrics.at(0)!.push(metric)
-  }
-}
-
-export function produce({
-  entity,
-  output,
-  itemType,
-  count,
-}: {
-  entity?: Entity
-  output: BufferEntity
-  itemType: ItemType
-  count: number
-}): void {
-  let value = output.contents[itemType]
-  if (!value) {
-    value = output.contents[itemType] = {
-      condition: 1,
-      count: 0,
-    }
-  }
-  value.count += count
-
-  if (entity) {
-    const metric: ProduceItemTickMetric = {
-      type: TickMetricType.enum.ProduceItem,
-      targetEntityId: output.id,
-      itemType,
-      count,
-    }
-    entity.metrics.at(0)!.push(metric)
   }
 }
 
